7938269181212c147c8ef59588bba0d4f80769ab
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-14 06:15:41 -0500

    picassolte: Rebrand cm -> lineage
    Change-Id: I50bd245518253061b4bba9f93195202bf4da7a33
    (cherry picked from commit 772a20c450d27429eb5fd9adbfe4fbc87dab12fa)

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/picassolte/full_picassolte.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := picassolte
